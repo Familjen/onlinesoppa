3819ea4c5be2d0f1302dca40da64fa3a6e2d657e
Maakuz <markuswerdien@hotmail.com>
2016-11-19 15:44:06 +0100

    Made UserMapper work

--- src/main/java/se/onlinesoppa/DBObject/user/UserMapper.java
+++ src/main/java/se/onlinesoppa/DBObject/user/UserMapper.java
@@ -3,5 +3,20 @@ package se.onlinesoppa.DBObject.user;
 /**
  * Created by Morgan on 2016-11-19.
  */
-public class UserMapper {
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+public class UserMapper implements RowMapper<User>{
+    public User mapRow(ResultSet rs, int rowNr) throws SQLException
+    {
+        User user = new User();
+
+        user.setId(rs.getInt("id"));
+        user.setPassword(rs.getString("password"));
+        user.setUsername(rs.getString("username"));
+
+        return user;
+    }
 }
