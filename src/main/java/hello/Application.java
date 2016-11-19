7877b97c00161d9cceadeac762e92da6a3ffeba0
Morgan Cromell <morgan.cromell@hotmail.com>
2016-11-19 14:08:45 +0100

    Added JDBC

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,11 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
+
         SpringApplication.run(Application.class, args);
     }
 }
